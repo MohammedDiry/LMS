bcfefad4e743201de6dc53656a6dd64fe673fab5
mohammedDiry <eng.mohammed.diry@gmail.com>
2026-02-25 23:52:06 +0300

    LMS Routing __ Mohammed Diry

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', [HomeController::class , 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('/books', BookController::class);
+Route::resource('/roles', RoleController::class);
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/profile', [UserController::class, 'showProfile'])->name('profile');
